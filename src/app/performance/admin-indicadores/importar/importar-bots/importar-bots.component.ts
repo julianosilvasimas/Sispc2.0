4bc8d8a52cb3b81878ca5e20d873a0292741a71e
VitorHeser <54639202+VitorHeser@users.noreply.github.com>
2020-03-09 09:45:36 -0300

    Update importar-bots.component.ts

--- src/app/performance/admin-indicadores/importar/importar-bots/importar-bots.component.ts
+++ src/app/performance/admin-indicadores/importar/importar-bots/importar-bots.component.ts
@@ -21,9 +21,9 @@ export class ImportarBotsComponent implements OnInit {
 
   ngOnInit() {
     this.bots =[
-      { label: "Inserir Orçados", value: 1 , funcao: "Carregar volume produzido", status: "S/I"},
-      { label: "Calculos Energia", value: 1 , funcao: "kW/m³, R$/m³ ", status: "S/I"},
-      { label: "Inserir Orçados", value: 1 , funcao: "Carregar volume produzido", status: "S/I"},
+      { label: "Atualizar Volumes", value: 1 , funcao: "Carregar volume produzido", status: "S/I"},
+      { label: "Calculos Energia", value: 2 , funcao: "kW/m³, R$/m³ ", status: "S/I"},
+      { label: "Calculos de Produtos Quimicos", value: 3 , funcao: "Carregar volume produzido", status: "S/I"},
     ] 
     this.botsLista = this.bots
     console.log(this.botsLista)
@@ -42,8 +42,14 @@ export class ImportarBotsComponent implements OnInit {
   }
   
   StartBot(bot){
-    this.adminindic.startBot(bot.value).subscribe();
-    this.bots[0].status = "Executando";
-    this.messageService.add({severity: 'success', summary: 'Informação', detail: 'Executando.'});
+    if(bot.status==="Pronto"){
+      this.adminindic.startBot(bot.value).subscribe();
+      this.bots[0].status = "Executando";
+      this.messageService.add({severity: 'success', summary: 'Informação', detail: 'Executando.'});
+    }else if(bot.status==="Executando"){
+      this.messageService.add({severity: 'info', summary: 'Informação', detail: 'O robot já está em execução.'});
+    }else if(bot.status==="S/I"){
+      this.messageService.add({severity: 'info', summary: 'Informação', detail: 'O robot ainda não está completo.'});
+    }
   }
 }
